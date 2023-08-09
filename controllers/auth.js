93c60f6d10e7b67755b7ae36eeca364f844a545e
Tim Hoerner <thoerner@gmail.com>
2023-08-09 17:57:24 -0400

    add verifySignature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,32 @@
 import jsonwebtoken from 'jsonwebtoken'
+import { verifyMessage } from 'ethers'
+import redis from '../services/redisService.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
 export const getToken = async (req, res) => {
-    const token = jsonwebtoken.sign({ address: req.query.address }, process.env.JWT_SECRET)
+    const { address } = req.query
+    const token = jsonwebtoken.sign({ address }, process.env.JWT_SECRET)
+    await redis.set(address, token)
     res.json({ token })
+}
+
+export const verifySignature = async (req, res) => {
+    const { address, signature, token } = req.body
+    const storedToken = await redis.get(address)
+    if (token !== storedToken) {
+        res.status(401).json({ message: "Token is invalid" })
+        return
+    }
+    const message = `You must sign this message to prove ownership of your wallet address.\n${token}`
+    const recoveredAddress = verifyMessage(message, signature)
+    console.log(recoveredAddress)
+    if (recoveredAddress === address) {
+        res.json({ 
+            success: true,
+            message: "Signature is valid" 
+        })
+    } else {
+        res.status(401).json({ message: "Signature is invalid" })
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,21 @@
 import express from "express"
 import { check } from "express-validator"
-import { getToken } from "../controllers/auth.js"
+import { getToken, verifySignature } from "../controllers/auth.js"
+
+const validateAddressFormat = (address) => {
+    return /^0x[a-fA-F0-9]{40}$/.test(address)
+}
+
+const validateSignatureFormat = (signature) => {
+    return /^0x[a-fA-F0-9]{130}$/.test(signature)
+}
 
 const router = express.Router()
 
 router.get("/token", getToken)
+router.post("/verify", [
+    check("address", "Address is invalid").custom(validateAddressFormat),
+    check("signature", "Signature is invalid").custom(validateSignatureFormat)
+], verifySignature)
 
 export default router
