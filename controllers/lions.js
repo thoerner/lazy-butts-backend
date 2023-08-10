0e68e8aa864f3e566bb42f390eaadb0025deee46
Tim Hoerner <thoerner@gmail.com>
2023-08-10 11:57:29 -0400

    add dev env

--- controllers/lions.js
+++ controllers/lions.js
@@ -3,13 +3,15 @@ import { validationResult } from 'express-validator'
 import ethProvider from '../services/ethService.js'
 import LazyLions from '../contracts/LazyLions.json' assert { type: "json" }
 
+const LION_CONTRACT_ADDRESS = process.env.ENV === 'dev' ? process.env.LION_CONTRACT_ADDRESS_TEST : process.env.LION_CONTRACT_ADDRESS
+
 export const getLions = async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() })
     }
     const { address } = req.params
-    const lionContract = new ethers.Contract(process.env.LION_CONTRACT_ADDRESS, LazyLions, ethProvider)
+    const lionContract = new ethers.Contract(LION_CONTRACT_ADDRESS, LazyLions, ethProvider)
     const lions = await lionContract.tokensOfOwner(address)
     const lionIds = lions.map(lion => lion.toString())
     res.json(lionIds)
--- services/ethService.js
+++ services/ethService.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const provider = new JsonRpcProvider(process.env.RPC_URL)
+const RPC_URL = process.env.ENV === 'dev' ? process.env.RPC_URL_TEST : process.env.RPC_URL
+const provider = new JsonRpcProvider(RPC_URL)
 
 export default provider
